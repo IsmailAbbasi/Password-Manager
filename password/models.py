030af23e52b92d8e21426593acb35a9b79819f27
IsmailAbbasi <ismailabbasi118@gmail.com>
2024-05-24 09:54:23 +0530

    PAssword and usernsame are shwoing on home page after submitting in database and added url section

--- password/models.py
+++ password/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Password(models.Model):
     pub_date = models.DateTimeField("Date published", auto_now_add=True)
     password = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
     choice_text = models.CharField(max_length=100)
 
     def __str__(self):
--- password/views.py
+++ password/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from .models import Password
+
+# def menu(request):
+#     if request.method == "POST":
+#         username = request.POST['username','']
+#         url = request.POST['url','']
+#         password = request.POST['pwd','']
+#         Password.objects.create(url=url,username=username, password=password)
+#         return redirect('menu')  # Redirect to the same page to prevent form resubmission
+#     entries = Password.objects.all()
+#     return render(request, 'password.html', {'entries': entries})
+
+# def notes(request):
+#     return render(request, 'notes.html')
+
+from django.shortcuts import render, redirect
 from .models import Password
 
 def menu(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['pwd']
-        print("Submit")
-        Password.objects.create(url=username, password=password)
-    # return HttpResponse("Hello, world. You're at the index.")
-    return render(request, 'password.html')
+        # Use request.POST.get() to handle the case where the key is missing or its value is empty
+        username = request.POST.get('username', '')  
+        url = request.POST.get('url', '')  
+        password = request.POST.get('pwd', '')  
+        
+       
+        if username:
+            Password.objects.create(url=url, username=username, password=password)
+            return redirect('menu')  
+        else:
+           
+            pass
+        
+    entries = Password.objects.all()
+    return render(request, 'password.html', {'entries': entries})
+
 def notes(request):
     return render(request, 'notes.html')
-def passwords(request):
-    return render(request, 'password.html')
