cd864f68053fd514393cf1f67c07b39dbcb19a65
IsmailAbbasi <ismailabbasi118@gmail.com>
2024-05-30 20:10:06 +0530

    Added encryption on users database so password that a user will save on database will be saved in encrypted form

--- password/models.py
+++ password/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 class Password(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1,related_name='passwords')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name='passwords')
     pub_date = models.DateTimeField("Date published", auto_now_add=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=500)  # Adjusted to accommodate encrypted data
     username = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
-    choice_text = models.CharField(max_length=100,blank=True, null=True)
+    choice_text = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.password
--- password/views.py
+++ password/views.py
@@ -1,10 +1,10 @@
-
+from django.conf import settings
 from .models import Password
 from django.shortcuts import render, redirect
-# from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm , UserCreationForm
 from .forms import UserRegisterForm
+from cryptography.fernet import Fernet
 
 def signupPage(request):
     if request.method == 'POST':
@@ -40,24 +40,68 @@ def logoutPage(request):
         auth_logout(request)
         return redirect('login') 
 
+# def menu(request):
+#     if request.user.is_authenticated == False:
+#         return redirect('login')
+#     if request.method == "POST":
+#         username = request.POST.get('username', '')  
+#         url = request.POST.get('url', '')  
+#         password = request.POST.get('pwd', '')  
+#         choice_text = request.POST.get('choice_text', '')
+       
+#         if username:
+#             Password.objects.create(url=url, username=username, password=password,user=request.user, choice_text=choice_text)
+#             return redirect('menu')  
+#         else:
+           
+#             pass
+        
+#     entries = Password.objects.filter(user=request.user)
+#     return render(request, 'password.html', {'entries': entries})
+
 def menu(request):
-    if request.user.is_authenticated == False:
+    if not request.user.is_authenticated:
         return redirect('login')
     if request.method == "POST":
         username = request.POST.get('username', '')  
         url = request.POST.get('url', '')  
         password = request.POST.get('pwd', '')  
         choice_text = request.POST.get('choice_text', '')
-       
+
+        # Encrypt the password
+        cipher_suite = Fernet(settings.FERNET_KEY.encode()) 
+        encrypted_password = cipher_suite.encrypt(password.encode())
+
+
         if username:
-            Password.objects.create(url=url, username=username, password=password,user=request.user, choice_text=choice_text)
+            Password.objects.create(url=url, username=username, password=encrypted_password.decode(), user=request.user, choice_text=choice_text)
             return redirect('menu')  
-        else:
-           
-            pass
-        
+        else:  
+           pass
+     
     entries = Password.objects.filter(user=request.user)
-    return render(request, 'password.html', {'entries': entries})
+    cipher_suite = Fernet(settings.FERNET_KEY.encode())
+    decrypted_entries = []
+    for entry in entries:
+        try:
+            decrypted_password = cipher_suite.decrypt(entry.password.encode()).decode()
+            decrypted_entry = {
+                'url': entry.url,
+                'username': entry.username,
+                'password': decrypted_password,
+                'choice_text': entry.choice_text,
+            }
+            decrypted_entries.append(decrypted_entry)
+        except Exception:
+            decrypted_entry = {
+                'url': entry.url,
+                'username': entry.username,
+                'password': 'Decryption error',
+                'choice_text': entry.choice_text,
+            }
+            decrypted_entries.append(decrypted_entry)
+
+    return render(request, 'password.html', {'entries': decrypted_entries})
 
 def notes(request):
     return render(request, 'notes.html')
