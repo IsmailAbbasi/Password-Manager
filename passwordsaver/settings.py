74baf226acbda49ebe74eb4b49afbbcfc557d8f9
IsmailAbbasi <ismailabbasi118@gmail.com>
2024-06-10 21:28:22 +0530

    Configure environment variables and deployment settings

--- passwordsaver/settings.py
+++ passwordsaver/settings.py
@@ -14,13 +14,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 # import django_heroku
-# import dj_databae_url
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['keycryptor.herokuapp.com']
 
 try:
     from .settings_local import *
@@ -84,6 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+            'default': dj_database_url.config(conn_max_age=600)
     }
 }
 
