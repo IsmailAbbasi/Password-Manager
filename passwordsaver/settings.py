219fbbf771202be5023537c8957191c7e0a97cf1
IsmailAbbasi <ismailabbasi118@gmail.com>
2024-06-11 17:10:54 +0530

    Use environment variables for sensitive settings

--- passwordsaver/settings.py
+++ passwordsaver/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'password' / 'static',
+    BASE_DIR / 'passwordsaver' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
