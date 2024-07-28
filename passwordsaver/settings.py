3588072d759f51fcba2197446e0fb271c8a8f572
IsmailAbbasi <ismailabbasi118@gmail.com>
2024-07-29 01:02:17 +0530

    Added izzy.fun to allowed hosts

--- passwordsaver/settings.py
+++ passwordsaver/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['keycryptor.herokuapp.com','127.0.0.1','keycryptor-926c7cc2b1ab.herokuapp.com',]
+ALLOWED_HOSTS = ['keycryptor.herokuapp.com','127.0.0.1','keycryptor-926c7cc2b1ab.herokuapp.com','izzy.fun']
 
 try:
     from .settings_local import *
